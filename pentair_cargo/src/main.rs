1744fa4303bf75263fa68d7b9b3f30fc8b577e69
Max Gubin <max@gubin.info>
2023-06-19 08:54:10 -0700

    Demo Web server, started configuration

---
+++ pentair_cargo/src/main.rs
@@ -0,0 +1,31 @@
+use std::{convert::Infallible, net::SocketAddr, error::Error};
+use http_body_util::Full;
+use hyper::{Request, Response, body::Bytes, service::service_fn};
+use hyper::server::conn::http1;
+use tokio::net::TcpListener;
+
+async fn hello(
+    _: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
