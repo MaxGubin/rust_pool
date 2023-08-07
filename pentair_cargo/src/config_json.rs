7a67574e437efee7fa3d0f04809316a4b472d76d
Max Gubin <max@gubin.info>
2023-08-06 20:38:32 -0700

    Started protocol

---
+++ pentair_cargo/src/config_json.rs
@@ -0,0 +1,13 @@
+// Controller parameters
+pub struct Controller {
+    
+}
+
+pub struct Comms {
+    pub type: String,
+    pub port: u32,
+    pub enabled: bool,
+    
+}
+pub struct config {
+    pub 
